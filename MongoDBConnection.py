cb6c265baefe94113a85397d01539f9d713de5e0
connorkoch0511 <connor.koch@student.csulb.edu>
2023-05-02 22:31:08 -0700

    fixed some bugs in code

--- MongoDBConnection.py
+++ MongoDBConnection.py
@@ -1,134 +1,75 @@
 from pymongo import MongoClient, database 
-
 import subprocess 
-
 import threading 
-
 import pymongo 
-
 from datetime import datetime, timedelta 
-
 import time 
-
- 
- 
+import certifi
 
 DBName = "test" #Use this to change which Database we're accessing 
-
 connectionURL = "mongodb+srv://connorkoch0511:SixersFan@cecs327assignment7.j3sdksp.mongodb.net/?retryWrites=true&w=majority" #Put your database URL here 
-
 sensorTable = "test.traffic data" #Change this to the name of your sensor data table 
 
- 
- 
-
 def QueryToList(query): 
-
     queryData = [] 
-
     for line in query: 
-
         queryData.append(line) 
-
     return queryData 
 
- 
- 
-
-  #pass; #TODO: Convert the query that you get in this function to a list and return it 
-
-  #HINT: MongoDB queries are iterable 
-
- 
- 
-
 def QueryDatabase() -> list: 
-
     global DBName 
-
     global connectionURL 
-
     global currentDBName 
-
     global running 
-
     global filterTime 
-
     global sensorTable 
-
     cluster = None 
-
     client = None 
-
     db = None 
 
     try: 
-
         cluster = connectionURL 
-
-        client = MongoClient(cluster) 
-
+        client = MongoClient(cluster, tlsCAFile=certifi.where()) 
         db = client[DBName] 
-
         print("Database collections: ", db.list_collection_names()) 
 
- 
- 
-
         #We first ask the user which collection they'd like to draw from. 
-
         sensorTable = db[sensorTable] 
-
         print("Table:", sensorTable) 
-
         #We convert the cursor that mongo gives us to a list for easier iteration. 
-
-        timeCutOff = datetime.now() - timedelta(minutes=0) #TODO: Set how many minutes you allow 
-
- 
- 
+        timeCutOff = datetime.now() - timedelta(minutes=200) #TODO: Set how many minutes you allow 
 
         oldDocuments = QueryToList(sensorTable.find({"time":{"$gte":timeCutOff}})) 
-
         currentDocuments = QueryToList(sensorTable.find({"time":{"$lte":timeCutOff}})) 
 
- 
- 
-
         print("Current Docs:",currentDocuments) 
-
         print("Old Docs:",oldDocuments) 
-
- 
- 
 
         #TODO: Parse the documents that you get back for the sensor data that you need 
 
-        for document in oldDocuments: 
+        RoadA = 0
+        RoadB = 0 
+        RoadC = 0
 
-            print(document['payload']) 
+        for document in oldDocuments: 
+            RoadA += document['payload']['Traffic Sensor 91']
+        
+        RoadA = RoadA / len(oldDocuments)
+
+        for document in oldDocuments: 
+            RoadB += document['payload']['Traffic Sensor 92']
+        
+        RoadB = RoadB / len(oldDocuments)
+
+        for document in oldDocuments: 
+            RoadC += document['payload']['Traffic Sensor 93']
+        
+        RoadC = RoadC / len(oldDocuments)
 
         #Return that sensor data as a list 
-
- 
- 
-
-        return oldDocuments 
-
- 
- 
- 
+        return [int(RoadA), int(RoadB), int(RoadC)] 
 
     except Exception as e: 
-
         print("Please make sure that this machine's IP has access to MongoDB.") 
-
         print("Error:",e) 
-
         exit(0) 
-
- 
- 
- 
-
- 
--- Server.py
+++ Server.py
@@ -1,155 +1,80 @@
 import socket 
-
 import ipaddress 
-
 import threading 
-
 import time 
-
 import contextlib 
-
 import errno 
-
 from dataclasses import dataclass 
-
 import random 
-
 import sys 
 
- 
- 
-
 maxPacketSize = 1024 
-
 defaultPort = 5050 #TODO: Set this to your preferred port 
 
- 
- 
-
 def GetFreePort(minPort: int = 1024, maxPort: int = 65535): 
-
     for i in range(minPort, maxPort): 
-
         print("Testing port",i) 
 
         with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as potentialPort: 
-
             try: 
-
                 potentialPort.bind(('localhost', i)) 
-
                 potentialPort.close() 
-
                 print("Server listening on port",i) 
 
                 return i 
-
             except socket.error as e: 
-
                 if e.errno == errno.EADDRINUSE: 
-
                     print("Port",i,"already in use. Checking next...") 
-
                 else: 
-
-                    print("An exotic error occurred:",e) 
-
- 
- 
+                    print("An exotic error occurred:",e)
 
 def GetServerData() -> list: 
-
     import MongoDBConnection as mongo 
-
     return mongo.QueryDatabase() 
 
- 
- 
- 
- 
- 
- 
- 
-
 def ListenOnTCP(tcpSocket: socket.socket, socketAddress): 
+    message = tcpSocket.recv(64).decode()
 
-    with tcpSocket: 
-
+    if message: 
         serverData = GetServerData() 
 
-        print(f"Connected by {socketAddress}") 
+        fastestHighway = min(serverData)
+        letter = "A"
 
- 
- 
+        if serverData[0] == fastestHighway:
+            letter = "A"
+        elif serverData[1] == fastestHighway:
+            letter = "B"
+        elif serverData[2] == fastestHighway:
+            letter = "C"
 
-        while True: 
-
-            data = tcpSocket.recv(64).decode('utf-8') 
-
-            if not data: 
-
-                break 
-
- 
- 
-
-            #send message to client 
-
-            tcpSocket.sendall(bytes([0, 1, 2], 'utf-8')) 
-
-    #pass; #TODO: Implement TCP Code, use GetServerData to query the database. 
-
- 
- 
- 
+        #send message to client 
+        tcpSocket.sendall(bytes(letter, 'utf-8'))
 
 def CreateTCPSocket() -> socket.socket: 
 
     tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
     #tcpPort = defaultPort 
-
     tcpPort = GetFreePort() 
-
     print("TCP Port:",tcpPort) 
-
     tcpSocket.bind(('localhost', tcpPort)) 
-
     return tcpSocket 
 
- 
- 
-
 def LaunchTCPThreads(): 
-
     tcpSocket = CreateTCPSocket() 
-
     tcpSocket.listen(5) 
 
     while True: 
-
         connectionSocket, connectionAddress = tcpSocket.accept() 
-
         connectionThread = threading.Thread(target=ListenOnTCP, args=[connectionSocket, connectionAddress]) 
-
         connectionThread.start() 
 
- 
- 
-
 if __name__ == "__main__": 
-
     tcpThread = threading.Thread(target=LaunchTCPThreads) 
-
     tcpThread.start() 
 
- 
- 
-
     while tcpThread.is_alive(): 
-
         time.sleep(1) 
-
     print("Ending program by exit signal...") 
 
  
